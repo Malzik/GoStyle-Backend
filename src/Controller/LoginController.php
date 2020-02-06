541f6a3b2095794b9bac80d20b6436d841ad3560
Alexis <alexis.heroin@epsi.fr>
2020-02-06 17:57:49 +0100

    correction routes login for swagger

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Swagger\Annotations as SWG;
 
 /**
  * Class LoginController
@@ -18,5 +19,24 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
  */
 class LoginController extends AbstractController
 {
-
+    /**
+     * @Route("/login", name="login", methods={"POST"})
+     * * @SWG\Response(
+     *     response=200,
+     *     description="Login",
+     * )
+     * @SWG\Parameter(
+     *     name="User",
+     *     in="body",
+     *     description="The field used to login",
+     *     @SWG\Schema(
+     *         type="object",
+     *         @SWG\Property(property="email", type="string"),
+     *         @SWG\Property(property="password", type="string")
+     *     )
+     * )
+     * @SWG\Tag(name="Users")
+     */
+    public function login() {
+    }
 }
